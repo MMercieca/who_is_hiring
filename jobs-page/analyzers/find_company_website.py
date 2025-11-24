fac51f1c039450762a3cbc675b4fd206fa3572d2
Matt Mercieca <mercieca@gmail.com>
2025-11-24 13:01:43 -0500

    Improving prompt for finding the initial company website

--- jobs-page/analyzers/find_company_website.py
+++ jobs-page/analyzers/find_company_website.py
@@ -8,8 +8,13 @@ def find_company_website(target_company):
     You are a corporate research assistant.  Search the web to find the official company website
     of the company listed below.
 
-    Be careful.  Find the company website, not reviews or ads.  
+    Some guidelines to identify the company website:
 
+    1. If the company name has a strange spelling, the website may be the company name followed by `.com`
+    2. If the company name has a normal spelling, the website may the company name with no spaces followed by a `.com`
+    3. If the company name is long some words of the name may be omitted.
+    4. Sometimes spaces are replaced with dashes.
+    
     Return ONLY the URL of the company website. Do not explain. If it is not found, return "None".
 
     Company:
